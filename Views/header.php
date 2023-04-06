8fc9b2cb452c4850ac5e85ff5538f2ffa0b99165
Michał <cienkowski.michal@gmail.com>
2023-04-06 14:25:26 +0200

    Creating Views folder and "header.php" file.

---
+++ Views/header.php
@@ -0,0 +1,15 @@
+<?php
+echo <<<END
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+<header>
+<p>Recipello</p>
+<a href="">Account</a>
+</header>
+
+END;
