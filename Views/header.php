f920fc3ae635c2c2cc90b1b651ebc4536fca381a
Michał <cienkowski.michal@gmail.com>
2023-04-06 14:32:15 +0200

    Creating CSS folder and "header.css" file. Updating "header.php".

--- Views/header.php
+++ Views/header.php
@@ -4,12 +4,14 @@ echo <<<END
 <html lang="eng">
 <head>
 <title>Title of the document</title>
+ <link rel="stylesheet" href="CSS/header.css"> 
 </head>
 
 <body>
 <header>
 <p>Recipello</p>
 <a href="">Account</a>
+<i class="fa-solid fa-user" style="color: #ffffff;"></i>
 </header>
 
 END;
