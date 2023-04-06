94389b11167202d944e763dd5992cc2762552acf
Michał <cienkowski.michal@gmail.com>
2023-04-06 15:07:02 +0200

    Changing CSS folder location and updating header files.

--- Views/header.php
+++ Views/header.php
@@ -1,17 +1,18 @@
-<?php
-echo <<<END
 <!DOCTYPE html>
 <html lang="eng">
 <head>
-<title>Title of the document</title>
- <link rel="stylesheet" href="CSS/header.css"> 
+    <title>Title of the document</title>
+    <link rel="stylesheet" href="CSS/header.css">
+    <script src="https://kit.fontawesome.com/5320478ffc.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
-<header>
-<p>Recipello</p>
-<a href="">Account</a>
-<i class="fa-solid fa-user" style="color: #ffffff;"></i>
-</header>
+<nav>
+    <div>Recipello</div>
+    <div>
+        <a href="">
+            Account
+            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+        </a>
+    </div>
 
-END;
+</nav>
