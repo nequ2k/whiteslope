fa80759599f4b19c5aa9d87ba009631cca7751a9
Michał <cienkowski.michal@gmail.com>
2023-04-13 15:45:59 +0200

    WHIT-23 Created "nav.php".

---
+++ Views/CSS/nav.php
@@ -0,0 +1,9 @@
+<nav class="col-xs-12 col-sm-8 col-md-6 col-lg-4">
+    <div>Recipello</div>
+    <div>
+        <a href="">
+            Account
+            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+        </a>
+    </div>
+</nav>
