c063bdccc362dbb50c2f9b11b3478f2fe9ae6b97
Michał <cienkowski.michal@gmail.com>
2023-04-13 15:39:56 +0200

    WHIT-23 Modified header and register files. Created "nav.php".

--- Views/header.php
+++ Views/header.php
@@ -2,20 +2,12 @@
 <html lang="eng">
 <head>
     <title>Title of the document</title>
-    <link rel="stylesheet" href="CSS/header.css"> <!-- css -->
     <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet"> <!-- fonts -->
     <script src="https://kit.fontawesome.com/5320478ffc.js" crossorigin="anonymous"></script> <!-- icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+    <link rel="stylesheet" href="CSS/header.css"> <!-- css -->
 </head>
 <body>
-<nav>
-    <div>Recipello</div>
-    <div>
-        <a href="">
-            Account
-            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
-        </a>
-    </div>
 
-</nav>
--- Views/register.php
+++ Views/register.php
@@ -1,34 +1,38 @@
 <?php require "header.php" ?>
 <link rel="stylesheet" href="CSS/register.css">
+
 <main>
-    <section>
-        <h1>Sign up</h1>
-        <form>
-            <label for="email">E-mail</label>
-            <input name="email" id="email" type="email" required="" placeholder="email@example.com">
-            <label class="red" for="email">E-mail already taken</label>
+    <div id="left" class="col-xs-12 col-sm-8 col-md-6 col-lg-6"></div>
+    <div id="right" class="col-xs-12 col-sm-8 col-md-6 col-lg-6">
+        <section class="col-xs-12 col-sm-8 col-md-6 col-lg-8">
+            <h3>Sign up</h3>
+            <form>
+                <label for="email">E-mail</label>
+                <input name="email" id="email" type="email" required="" placeholder="email@example.com">
+                <label class="red" for="email">E-mail already taken</label>
 
-            <label for="login">Login</label>
-            <input name="login" id="login" type="text" required="" placeholder="User123#!">
-            <label class="red" for="email">Login already taken</label>
+                <label for="login">Login</label>
+                <input name="login" id="login" type="text" required="" placeholder="User123#!">
+                <label class="red" for="email">Login already taken</label>
 
-            <label for="password">Password</label>
-            <input name="password" id="password" type="password" required="" placeholder="password">
-            <label class="red" for="email">Password must be minimum 8 characters long and have uppercase letters, lowercase letters, numbers and symbols</label>
+                <label for="password">Password</label>
+                <input name="password" id="password" type="password" required="">
+                <label class="red" for="email">Password must be minimum 8 characters long and have uppercase letters, lowercase letters, numbers and symbols</label>
 
-            <label for="confirm_password">Confirm Password</label>
-            <input name="confirm_password" id="confirm_password" type="password" required="" placeholder="password">
-            <label class="red" for="email">Passwords don`t match</label>
+                <label for="confirm_password">Confirm Password</label>
+                <input name="confirm_password" id="confirm_password" type="password" required="">
+                <label class="red" for="email">Passwords don`t match</label>
 
-            <button type="submit">Register</button>
-        </form>
-        <div>
-            <a href="login.php">
-                Already have an account? Log in!
-                <i class="fa-solid fa-arrow-right" style="color: #000000;"></i>
-            </a>
-        </div>
-    </section>
+                <button type="submit">Register</button>
+            </form>
+            <div>
+                <a href="login.php">
+                    Already have an account? Log in!
+                    <i class="fa-solid fa-arrow-right" style="color: #ffffff;"></i>
+                </a>
+            </div>
+        </section>
+    </div>
 </main>
 
 
