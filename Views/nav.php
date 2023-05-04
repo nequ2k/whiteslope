0ef37e4598fe2a5964dbd44c5d6e20b7170de42d
Michał <cienkowski.michal@gmail.com>
2023-05-04 16:49:07 +0200

    WHIT-34 Creating nav.php as navigation bar and css file for it.

--- Views/nav.php
+++ Views/nav.php
@@ -1,9 +1,44 @@
-<nav >
-    <div>Recipello</div>
-    <div>
+<?php require "header.php" ?>
+
+<link rel="stylesheet" href="CSS/nav.css">
+<nav class="col-xs-12 col-sm-8 col-md-6 col-lg-1">
+    <div class="logo">
+        <div class="rollback">Recipello</div>
+    </div>
+    <div class="nav_element" >
         <a href="">
-            Account
-            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+            <div class="rollback">Browse</div>
+            <i class="fa-solid fa-book"></i>
+        </a>
+    </div>
+    <div class="nav_element" >
+        <a href="">
+            <div class="rollback">Cook</div>
+            <i class="fa-solid fa-bowl-food"></i>
+        </a>
+    </div>
+    <div class="nav_element">
+        <a href="">
+            <div class="rollback">Favourites</div>
+            <i class="fa-solid fa-star"></i>
+        </a>
+    </div>
+    <div class="nav_element">
+        <a href="">
+            <div class="rollback">My recipes</div>
+            <i class="fa-regular fa-pen-to-square"></i>
+        </a>
+    </div>
+    <div class="nav_element" >
+        <a href="">
+            <div class="rollback">Account</div>
+            <i class="fa-solid fa-user"></i>
+        </a>
+    </div>
+    <div class="nav_element">
+        <a href="">
+            <div class="rollback">Log out</div>
+            <i class="fa-solid fa-right-from-bracket"></i>
         </a>
     </div>
 </nav>
