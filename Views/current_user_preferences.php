67a2042e16e494bd738acb4f168fcf1d7393c876
Michał <cienkowski.michal@gmail.com>
2023-05-30 13:13:40 +0200

    Adding current_user_favourites/preferences views.

---
+++ Views/current_user_preferences.php
@@ -0,0 +1,163 @@
+<?php require "header.php" ?>
+<?php require "nav.php" ?>
+
+<link rel="stylesheet" href="CSS/current_user_preferences.css">
+<main class="col-xs-12 col-sm-10 col-md-6 col-lg-11">
+    <form>
+        <section>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian Food lalala</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian Food</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian Food</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian Food lalala</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian Food lalala</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div><div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div><div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+            <div class="preferences">
+                <input type=checkbox checked="">
+                <label>Italian</label>
+            </div>
+        </section>
+
+
+        <button class="save_changes_button" type="submit">Save changes</button>
+    </form>
+</main>
