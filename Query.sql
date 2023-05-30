892857d314fc724798318cb8ab5d6f8bc308f817
BrownieAsd <129280045+BrownieAsd@users.noreply.github.com>
2023-05-30 15:08:45 +0200

    Create Query.sql

---
+++ Query.sql
@@ -0,0 +1,25 @@
+Seed Categories:
+INSERT INTO `categories`(`category_name`) VALUES ('Italian'),('Spanish'),('French'),('English'),('German'),('Polish'),('Mediterranean'),('Asian'),('Indian'),('Turkish'),('Japanese'),('American'),('Chinese'),('Pacific'),('Thai'),('Korean'),('African'),('Irish'),('Scandinavian'),('Sushi'),('Burgers'),('Pizza'),('Dessert');
+Seed Users:
+INSERT INTO users (users_id, users_email, users_user_name, users_password, likesHot, isVegan)
+VALUES
+  (1, 'user1@example.com', 'user1', 'password1', true, false),
+  (2, 'user2@example.com', 'user2', 'password2', false, true),
+  (3, 'user3@example.com', 'user3', 'password3', true, true),
+  (4, 'user4@example.com', 'user4', 'password4', false, false),
+  (5, 'user5@example.com', 'user5', 'password5', true, true),
+  (6, 'user6@example.com', 'user6', 'password6', false, false),
+  (7, 'user7@example.com', 'user7', 'password7', true, false),
+  (8, 'user8@example.com', 'user8', 'password8', false, true),
+  (9, 'user9@example.com', 'user9', 'password9', true, true),
+  (10, 'user10@example.com', 'user10', 'password10', false, false),
+  (11, 'user11@example.com', 'user11', 'password11', true, false),
+  (12, 'user12@example.com', 'user12', 'password12', false, true),
+  (13, 'user13@example.com', 'user13', 'password13', true, true),
+  (14, 'user14@example.com', 'user14', 'password14', false, false),
+  (15, 'user15@example.com', 'user15', 'password15', true, true),
+  (16, 'user16@example.com', 'user16', 'password16', false, false),
+  (17, 'user17@example.com', 'user17', 'password17', true, false),
+  (18, 'user18@example.com', 'user18', 'password18', false, true),
+  (19, 'user19@example.com', 'user19', 'password19', true, true),
+  (20, 'user20@example.com', 'user20', 'password20', false, false);
