5b0e2b106d5c751a4c8bdf815b3d8530080da2c6
Wiktor Balcerek <92432473+nequ2k@users.noreply.github.com>
2023-07-25 14:43:19 +0200

    fixed mismatches

--- Classes/user_recipe_classes.php
+++ Classes/user_recipe_classes.php
@@ -2,39 +2,39 @@
 require_once 'dbh_classes.php';
 class UserRecipe extends Dbh
 {
-private int $userId;
+    private int $userId;
 
-public function __construct(int $userId)
-{
-$this->userId = $userId;
-}
+    public function __construct(int $userId)
+    {
+        $this->userId = $userId;
+    }
 
-public function getUserRecipes(): array
-{
-$connection = $this->connect();
+    public function getUserRecipes(): array
+    {
+        $connection = $this->connect();
 
-$query = 'SELECT * FROM recipes WHERE user_id = :userId';
-$stmt = $connection->prepare($query);
-$stmt->bindValue(':userId', $this->userId, PDO::PARAM_INT);
-$stmt->execute();
-$recipesData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $query = 'SELECT * FROM recipes WHERE user_id = :userId';
+        $stmt = $connection->prepare($query);
+        $stmt->bindValue(':userId', $this->userId, PDO::PARAM_INT);
+        $stmt->execute();
+        $recipesData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$recipes = [];
-foreach ($recipesData as $recipeData) {
-$recipe = new Recipe(
-$recipeData['title'],
-(int) $recipeData['category_id'],
-(int) $recipeData['isVegan'],
-(int) $recipeData['likesHot'],
-(float) $recipeData['rating'],
-explode(',', $recipeData['ingredients']),
-(int) $recipeData['user_id']
-);
-$recipes[] = $recipe;
-}
+        $recipes = [];
+        foreach ($recipesData as $recipeData) {
+        $recipe = new Recipe(
+        $recipeData['title'],
+        (int) $recipeData['category_id'],
+        (int) $recipeData['isVegan'],
+        (int) $recipeData['likesHot'],
+        (float) $recipeData['rating'],
+        explode(',', $recipeData['ingredients']),
+        (int) $recipeData['user_id']
+        );
+        $recipes[] = $recipe;
+        }
 
-return $recipes;
-}
+        return $recipes;
+    }
 }
 
 ?>
