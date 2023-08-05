68a4debd6cbd1653dd7d4996b03a035f5907aeb1
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-08-06 00:10:51 +0200

    Improve navbar WHIT-48
    Improved navbar should work now on all devices (responsive)
    Added hamburger button to page
    Changed background for navbar

--- Views/nav.php
+++ Views/nav.php
@@ -1,84 +1,53 @@
 <?php require "header.php" ?>
 
+<link rel="stylesheet" href="CSS/hamburgers.min.css">
 <link rel="stylesheet" href="CSS/nav.css">
-<nav class="col-xs-12 col-sm-2 col-md-6 col-lg-1">
-    <div class="logo">
-        <div class="rollback">Recipello</div>
-    </div>
-    <div class="nav_element" >
-        <a href="">
-            <div class="rollback">Browse</div>
-            <i class="fa-solid fa-book"></i>
-        </a>
-    </div>
-    <div class="nav_element" >
-        <a href="">
-            <div class="rollback">Cook</div>
-            <i class="fa-solid fa-bowl-food"></i>
-        </a>
-    </div>
-    <?php
-    if(isset($_SESSION['userid']))
-    {
-    ?>
-    <div class="nav_element">
-        <a href="">
-            <div class="rollback">Favourites</div>
-            <i class="fa-solid fa-star"></i>
-        </a>
-    </div>
-    <?php
-    }
-    if(isset($_SESSION['userid']))
-        {
-     ?>
-    <div class="nav_element">
-        <a href="my_recipes.php">
-            <div class="rollback">My recipes</div>
-            <i class="fa-regular fa-pen-to-square"></i>
-        </a>
-    </div>
-    <?php
-        }
-    if(isset($_SESSION['userid']))
-        {
-    ?>
-    <div class="nav_element" >
-        <a href="current_user.php">
-            <div class="rollback">Account</div>
-            <i class="fa-solid fa-user"></i>
-        </a>
-    </div>
-    <?php
-        }
-        else
-        {
-    ?>
-    <div class="nav_element" >
-        <a href="login.php">
-            <div class="rollback">Login</div>
-            <i class="fa-solid fa-user"></i>
-        </a>
-    </div>
-    <div class="nav_element" >
-        <a href="register.php">
-            <div class="rollback">Register now!</div>
-            <i class="fa-solid fa-user"></i>
-        </a>
-    </div>
-    <?php
-        }
 
-    if(isset($_SESSION['userid']))
-        {
-    ?>
-    <div class="nav_element">
-        <a href="../Includes/logout_includes.php">
-            <div class="rollback">Log out</div>
-            <i class="fa-solid fa-right-from-bracket"></i>
-        </a>
-    </div>
-    <?php
-        }
-    ?>
-</nav>
+<button class="hamburger hamburger--collapse" type="button">
+    <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+    </span>
+</button>
+
+<aside class="navigation">
+    <nav>
+        <h2>Recipello</h2>
+        <ul>
+            <li><a href="mainpage.php">Browse <i class="fa-solid fa-book"></i></a></li>
+            <li><a href="#">Cook <i class="fa-solid fa-bowl-food"></i></a></li>
+            <?php
+            if (isset($_SESSION['userid'])) {
+            ?>
+            <li><a href="#">Favourites <i class="fa-solid fa-star"></i></a></li>
+            <li><a href="my_recipes.php">My recipes <i class="fa-regular fa-pen-to-square"></i></a></li>
+            <li><a href="current_user.php">Account <i class="fa-solid fa-user"></i></a></li>
+            <?php
+            } else {
+            ?>
+            <li><a href="login.php">Login <i class="fa-solid fa-user"></i></a></li>
+            <li><a href="register.php">Register now! <i class="fa-solid fa-user"></i></a></li>
+            <?php
+            }
+            if (isset($_SESSION['userid'])) {
+            ?>
+            <li><a href="">Log out <i class="fa-solid fa-right-from-bracket"></i></a></li>
+            <?php
+            }
+            ?>
+        </ul>
+        <img src="Images/nav_bg.jpg" alt="nav_background">
+    </nav>
+</aside>
+
+<script>
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+var navigation = document.querySelector(".navigation");
+// On click
+hamburger.addEventListener("click", () => {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    navigation.classList.toggle("nav-active");
+});
+</script>
