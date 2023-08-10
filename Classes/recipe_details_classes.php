6a081e94993a61e4f0b7a8dea20f317fa53bb8f6
Michał Cienkowski <120496051+MCienkowski@users.noreply.github.com>
2023-08-10 11:17:52 +0200

    WHIT-59 Fixing user_recipes. I added the wrong file to commit 58,recipe_details.classes should be there and user_recipes_classes should be here

---
+++ Classes/recipe_details_classes.php
@@ -0,0 +1,37 @@
+<?php
+
+require 'recipe_classes.php';
+
+class Recipe_details extends Dbh
+{
+    public static function getRecipe(string $hidden_title,string $hidden_user_id):Recipe
+    {
+        $dbh = new Dbh();
+        $connection = $dbh->connect();
+
+        $query = "SELECT * FROM recipes WHERE user_id = :user_id AND title = :title";
+        $stmt = $connection->prepare($query);
+        $stmt->bindValue(':user_id', (int)$hidden_user_id, PDO::PARAM_INT);
+        $stmt->bindValue(':title', $hidden_title, PDO::PARAM_STR);
+//        $query = "SELECT * FROM recipes WHERE user_id = :user_id AND title = :title";
+//        $stmt = $connection->prepare($query);
+//        $stmt->bindParam('user_id', $hidden_user_id, PDO::PARAM_INT);
+//        $stmt->bindParam('title', $hidden_title, PDO::PARAM_STR);
+        $stmt->execute();
+        $recipeData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($recipeData){
+            $recipe = new Recipe(
+                $recipeData['title'],
+                (int) $recipeData['category_id'],
+                (int) $recipeData['isVegan'],
+                (int) $recipeData['likesHot'],
+                (int) $recipeData['time'],
+//                (float) $recipeData['rating'], must have rating in recipe_class
+                explode(',', $recipeData['ingredients']),
+                (int) $recipeData['user_id']
+            );
+        }
+        return $recipe;
+    }
+}
