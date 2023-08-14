e6f0d87ac8de0f9f63f7a6124845c2f0f4048bf3
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-08-14 11:10:54 +0200

    Responsive mainpage WHIT-47
    Added responsive layout to mainpage

--- Views/mainpage.php
+++ Views/mainpage.php
@@ -3,12 +3,13 @@
 <?php require "nav.php" ?>
 
 <link rel="stylesheet" href="CSS/mainpage.css">
+<link rel="stylesheet" href="CSS/body_grid.css">
 
-<main class="col-xs-12 col-sm-10 col-md-6 col-lg-11">
+<main>
 
-    <p>Popular <span class="chefs_p">chefs</span></p>
+    <h2>Popular <span class="chefs_p">chefs</span></h2>
 
-    <div class="popular_chefs">
+    <section class="popular_chefs">
 
         <a href="">
             <div class="popular_chef">
@@ -28,15 +29,16 @@
                 User123#1
             </div>
         </a>
-    </div>
+    </section>
 
     <form class="search_form" action="search_results.php" method="GET">
         <input type="search" placeholder="search" name="searchresults">
         <button type="submit" class="search_button"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
 
-    <p>Top trending receipes</p>
-    <div class="top_trending_recipes col-xs-12 col-sm-8 col-md-6 col-lg-12">
+    <h2>Top trending recipes</h2>
+    <section class="top_trending_recipes">
+
         <?php
         require_once '../Classes/recipe_classes.php';
         $recipes = Recipe::getTrendingRecipes(10);
@@ -44,65 +46,61 @@
         foreach ($recipes as $recipe) {
         ?>
 
-        <div class="top_trending_recipe">
-            <img src="../Views/Images/spaghetti_test.png">
-            <div class="mid">
-                <p class="name"><?php echo $recipe->getTitle(); ?></p>
-                <div class="inner_mid">
-                    <p class="details">
-                        <i class="fa-solid fa-user"></i>
-                        <?php echo $recipe->getUsername(); ?>
-                    </p>
-                    <p class="details">
-                        <i class="fa-solid fa-clock"></i>
-                        <?php echo $recipe->getTime(); ?> minutes
-                    </p>
-                    <p class="details">
-                        <i class="fa-solid fa-note-sticky"></i>
-                        <?php echo $recipe->getCategory(); ?>
-                    </p>
+        <div class="col-xs-12">
+            <article class="card">
+                <img src="../Views/Images/spaghetti_test.png" alt="">
+                <div class="card--content">
+                    <h2 class="card--title">
+                        <?php echo $recipe->getTitle(); ?>
+                    </h2>
+                    <div class="card--details">
+                        <p>
+                            <i class="fa-solid fa-user"></i>
+                            <?php echo $recipe->getUsername(); ?>
+                        </p>
+                        <p>
+                            <i class="fa-solid fa-clock"></i>
+                            <?php echo $recipe->getTime(); ?> minutes
+                        </p>
+                        <p>
+                            <i class="fa-solid fa-note-sticky"></i>
+                            <?php echo $recipe->getCategory(); ?>
+                        </p>
+                    </div>
+                    <div class="card--vegan_spicy">
+                        <div class="bg--gray <?php echo $recipe->getIsVegan() ? 'vegan--active' : ''; ?>">
+                            <i class="fa-solid fa-seedling"></i>
+                        </div>
+                        <div class="bg--gray <?php echo $recipe->getLikesHot() ? 'spicy--active' : ''; ?>">
+                            <i class="fa-solid fa-pepper-hot"></i>
+                        </div>
+                    </div>
+                    <div class="card--rating">
+                        <div class="box">
+                            <p class="card--score">
+                                <?php echo $recipe->getRating(); ?>
+                                <i class="fa-solid fa-star"></i>
+                            </p>
+                            <p class="card--number">47
+                                <?php // echo $recipe->getLikes(); 
+                                    ?>
+                                <i class="fa-solid fa-user"></i>
+                            </p>
+                        </div>
+                    </div>
+                    <form method="post" action="recipe_details.php">
+                        <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle() ?>">
+                        <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId() ?>">
+                        <button type="submit" name="recipe_details_submit" class="card--button">Details</button>
+                    </form>
                 </div>
-            </div>
-            <div class="right">
-                <div class="vegan_spicy">
-                    <div class="gray <?php echo $recipe->getIsVegan() ? 'green' : ''; ?>">
-                        <i class="fa-solid fa-seedling"
-                            style="color: <?php echo $recipe->getIsVegan() ? '#42f410' : ''; ?>;"></i>
-                    </div>
-                    <div class="gray">
-                        <i class="fa-solid fa-pepper-hot"
-                            style="color: <?php echo $recipe->getLikesHot() ? '#ff2600' : ''; ?>;"></i>
-                    </div>
-                </div>
-                <div class="rating">
-                    <div class="inner_rating">
-                        <?php  echo $recipe->getRating();
-                            ?>
-                        <i class="fa-solid fa-star" style="color: #ffea00;"></i>
-                    </div>
-                    <div class="inner_rating">
-                        <?php // echo $recipe->getLikes(); 
-                            ?>
-                        <i class="fa-solid fa-user"></i>
-                    </div>
-                </div>
-                <form method="post" action="recipe_details.php">
-                    <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle()?>">
-                    <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId()?>">
-                    <button type="submit" name="recipe_details_submit" class="details_button">Details</button>
-                </form>
-            </div>
+            </article>
         </div>
 
         <?php
         }
         ?>
 
-    </div>
-
-
-
-
-
+    </section>
 
 </main>
