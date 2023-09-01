47ae3da7c6a20ca17332961909f52cd286ac956d
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-09-01 20:07:07 +0200

    Recipe details 'you might like' section WHIT-42

--- Views/recipe_details.php
+++ Views/recipe_details.php
@@ -6,6 +6,9 @@
 
 
 
+<!-- temporary solution for trending recipes -->
+<link rel="stylesheet" href="CSS/mainpage.css">
+<!-- --- -->
 <link rel="stylesheet" href="CSS/recipe_details.css">
 <link rel="stylesheet" href="CSS/body_grid.css">
 
@@ -74,8 +77,71 @@
         <?php echo $recipe->getMethodOfPrep() ?>
     </div>
 
-    <!-- <div class="bottom_div">
-    </div> -->
+    <!-- Add function that matches other recipes with currently showed one -->
+    <section class="top_trending_recipes">
+        <h2>You might like...</h2>
 
+        <?php
+        require_once '../Classes/recipe_classes.php';
+        $recipes = Recipe::getTrendingRecipes();
+
+        foreach ($recipes as $recipe) {
+        ?>
+
+            <div class="col-xs-12">
+                <article class="card">
+                    <img src="../Views/Images/spaghetti_test.png" alt="">
+                    <div class="card--content">
+                        <h2 class="card--title">
+                            <?php echo $recipe->getTitle(); ?>
+                        </h2>
+                        <div class="card--details">
+                            <p>
+                                <i class="fa-solid fa-user"></i>
+                                <?php echo $recipe->getUsername(); ?>
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-clock"></i>
+                                <?php echo $recipe->getTime(); ?> minutes
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-note-sticky"></i>
+                                <?php echo $recipe->getCategoriesAsString(); ?>
+                            </p>
+                        </div>
+                        <div class="card--vegan_spicy">
+                            <div class="bg--gray <?php echo $recipe->getIsVegan() ? 'vegan--active' : ''; ?>">
+                                <i class="fa-solid fa-seedling"></i>
+                            </div>
+                            <div class="bg--gray <?php echo $recipe->getIsSpicy() ? 'spicy--active' : ''; ?>">
+                                <i class="fa-solid fa-pepper-hot"></i>
+                            </div>
+                        </div>
+                        <div class="card--rating">
+                            <div class="box">
+                                <p class="card--score">
+                                    <?php echo $recipe->getRating(); ?>
+                                    <i class="fa-solid fa-star"></i>
+                                </p>
+                                <p class="card--number">
+                                    <?php echo $recipe->getRatingUsersCount(); ?>
+                                    <i class="fa-solid fa-user"></i>
+                                </p>
+                            </div>
+                        </div>
+                        <form method="post" action="recipe_details.php">
+                            <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle() ?>">
+                            <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId() ?>">
+                            <button type="submit" name="recipe_details_submit" class="card--button">Details</button>
+                        </form>
+                    </div>
+                </article>
+            </div>
+
+        <?php
+        }
+        ?>
+
+    </section>
 
 </main>
