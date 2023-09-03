54412e1ee440b22db806e8623e8f642c5a44ae3c
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-09-03 19:47:22 +0200

    Shopping list WHIT-47

--- Views/shopping_list.php
+++ Views/shopping_list.php
@@ -1,10 +1,11 @@
 <?php session_start(); ?>
-<?php require "header.php" ?>
 <?php require "nav.php" ?>
 <?php require_once '../Classes/shopping_list_classes.php'; ?>
 
 
 <link rel="stylesheet" href="CSS/shopping_list.css">
+<link rel="stylesheet" href="CSS/body_grid.css">
+
 <main>
     <?php
     if (isset($_POST["remove_shopping_list_element"])) {
@@ -16,37 +17,36 @@
         header("location: ../Views/shopping_list.php");
     }
     ?>
-    <h1>Your shopping list:</h1>
+    <h1>Your shopping list</h1>
     <?php
-        if (isset($_POST["shopping_list_button"])) {
-            shopping_list_classes::addShoppingList($_POST["hidden_title"], $_POST["hidden_user_id"]);
-        }
+    if (isset($_POST["shopping_list_button"])) {
+        shopping_list_classes::addShoppingList($_POST["hidden_title"], $_POST["hidden_user_id"]);
+    }
 
-        if(isset($_SESSION['shopping_list'])){
-            foreach($_SESSION['shopping_list'] as $ingredient){
-                echo "
+    if (isset($_SESSION['shopping_list'])) {
+        foreach ($_SESSION['shopping_list'] as $ingredient) {
+            echo "
                 <form  action='shopping_list.php' method='post'>
                     <input type='hidden' name='shopping_list_element_to_remove' value='$ingredient'>
                 <div class='remove'>
+                <p>
                     $ingredient
+                    </p>
                     <button name='remove_shopping_list_element'><i class='fa-solid fa-trash'></i></button>
                 </div>
                 </form>
                 
                 ";
-            }
-            echo "
+        }
+        echo "
                 <form action='shopping_list.php' method='post'>
                     <button class='clear' name='clear_shopping_list'>Clear your shopping list</button>
                 </form>
                 <form>
                     <button class='search_in_markets'><i class='fa-solid fa-skull'></i>Search in markets<i class='fa-solid fa-skull'></i></button>
                 </form>";
-            
-        }
-        else echo "<p>You have not added anything to your shopping list yet</p>";
+    } else echo "<p>You have not added anything to your shopping list yet</p>";
 
     ?>
-    
-   
+
 </main>
