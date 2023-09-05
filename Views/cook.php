bf5f30b9aa4d2e69221449f5caf261e706269ba0
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-09-05 16:44:08 +0200

    Cook view WHIT-92

---
+++ Views/cook.php
@@ -0,0 +1,101 @@
+<?php session_start(); ?>
+
+<?php require "nav.php" ?>
+
+<link rel="stylesheet" href="CSS/cook.css">
+<link rel="stylesheet" href="CSS/body_grid.css">
+
+<main class="loggedIn">
+    <h1>Cook from your ingredients!</h1>
+    <section class="ingredients">
+        <form action="#">
+            <?php
+            require_once '../Classes/Ingredient_classes.php';
+            require_once '../Classes/Ingredient_category_classes.php';
+            $ingredients_categories = Ingredient::getAllCategories();
+            foreach ($ingredients_categories as $category) {
+                echo
+                "<ul class='ingredients_category col-xs-12'>
+            <h3>" . $category->getCategoryName() . "</h3>";
+                $ingredients = Ingredient::getIngredients($category->getCategoryId());
+                foreach ($ingredients as $ingredient) {
+                    echo "<li class='ingredients_category_item'>";
+                    echo "<input type='checkbox' name='ingredient[]' value='" . $ingredient->getName() . "'>";
+                    echo "<label>" . $ingredient->getName() . "</label>";
+                    echo "</li>";
+                }
+                echo "</ul>";
+            }
+            ?>
+            <button type="submit" class="btn-primary">Search</button>
+        </form>
+    </section>
+
+    <h2>This is what you can make</h2>
+    <section class="top_trending_recipes">
+
+        <?php
+        require_once '../Classes/recipe_classes.php';
+        $recipes = Recipe::getTrendingRecipes(5);
+
+        foreach ($recipes as $recipe) {
+        ?>
+
+            <div class="col-xs-12">
+                <article class="card">
+                    <img src="../Views/Images/spaghetti_test.png" alt="">
+                    <div class="card--content">
+                        <h2 class="card--title">
+                            <?php echo $recipe->getTitle(); ?>
+                        </h2>
+                        <div class="card--details">
+                            <p>
+                                <i class="fa-solid fa-user"></i>
+                                <?php echo $recipe->getUsername(); ?>
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-clock"></i>
+                                <?php echo $recipe->getTime(); ?> minutes
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-note-sticky"></i>
+                                <?php echo $recipe->getCategoriesAsString(); ?>
+                            </p>
+                        </div>
+                        <div class="card--vegan_spicy">
+                            <div class="bg--gray <?php echo $recipe->getIsVegan() ? 'vegan--active' : ''; ?>">
+                                <i class="fa-solid fa-seedling"></i>
+                            </div>
+                            <div class="bg--gray <?php echo $recipe->getIsSpicy() ? 'spicy--active' : ''; ?>">
+                                <i class="fa-solid fa-pepper-hot"></i>
+                            </div>
+                        </div>
+                        <div class="card--rating">
+                            <div class="box">
+                                <p class="card--score">
+                                    <?php echo $recipe->getRating(); ?>
+                                    <i class="fa-solid fa-star"></i>
+                                </p>
+                                <p class="card--number">
+                                    <?php echo $recipe->getRatingUsersCount(); ?>
+                                    <i class="fa-solid fa-user"></i>
+                                </p>
+                            </div>
+                        </div>
+                        <form method="post" action="recipe_details.php">
+                            <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle() ?>">
+                            <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId() ?>">
+                            <button type="submit" name="recipe_details_submit" class="card--button">Details</button>
+                        </form>
+                    </div>
+                </article>
+            </div>
+
+        <?php
+        }
+        ?>
+
+    </section>
+
+
+</main>
