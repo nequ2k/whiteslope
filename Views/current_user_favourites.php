b866258a02c209eb6d065f45952dab631991a06f
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-09-18 17:48:28 +0200

    Fix for removing from favourites

--- Views/current_user_favourites.php
+++ Views/current_user_favourites.php
@@ -23,61 +23,60 @@
         foreach ($recipes as $recipe) {
         ?>
 
-        <div class="col-xs-12">
-            <article class="card">
-                <img src="../Views/Images/spaghetti_test.png" alt="">
-                <div class="card--content">
-                    <h2 class="card--title">
-                        <?php echo $recipe->getTitle(); ?>
-                    </h2>
-                    <div class="card--details">
-                        <p>
-                            <i class="fa-solid fa-user"></i>
-                            <?php echo $recipe->getUsername(); ?>
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-clock"></i>
-                            <?php echo $recipe->getTime(); ?> minutes
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-note-sticky"></i>
-                            <?php echo $recipe->getCategoriesAsString(); ?>
-                        </p>
-                    </div>
-                    <div class="card--vegan_spicy">
-                        <div class="bg--gray <?php echo $recipe->getIsVegan() ? 'vegan--active' : ''; ?>">
-                            <i class="fa-solid fa-seedling"></i>
-                        </div>
-                        <div class="bg--gray <?php echo $recipe->getIsSpicy() ? 'spicy--active' : ''; ?>">
-                            <i class="fa-solid fa-pepper-hot"></i>
-                        </div>
-                    </div>
-                    <div class="card--rating">
-                        <div class="box">
-                            <p class="card--score">
-                                <?php echo $recipe->getRating(); ?>
-                                <i class="fa-solid fa-star"></i>
-                            </p>
-                            <p class="card--number">
-                                <?php echo $recipe->getRatingUsersCount(); ?>
+            <div class="col-xs-12">
+                <article class="card">
+                    <img src="../Views/Images/spaghetti_test.png" alt="">
+                    <div class="card--content">
+                        <h2 class="card--title">
+                            <?php echo $recipe->getTitle(); ?>
+                        </h2>
+                        <div class="card--details">
+                            <p>
                                 <i class="fa-solid fa-user"></i>
+                                <?php echo $recipe->getUsername(); ?>
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-clock"></i>
+                                <?php echo $recipe->getTime(); ?> minutes
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-note-sticky"></i>
+                                <?php echo $recipe->getCategoriesAsString(); ?>
                             </p>
                         </div>
-                    </div>
-                    <form method="post" action="recipe_details.php">
-                        <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle() ?>">
-                        <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId() ?>">
-                        <button type="submit" name="recipe_details_submit" class="card--button">Details</button>
+                        <div class="card--vegan_spicy">
+                            <div class="bg--gray <?php echo $recipe->getIsVegan() ? 'vegan--active' : ''; ?>">
+                                <i class="fa-solid fa-seedling"></i>
+                            </div>
+                            <div class="bg--gray <?php echo $recipe->getIsSpicy() ? 'spicy--active' : ''; ?>">
+                                <i class="fa-solid fa-pepper-hot"></i>
+                            </div>
+                        </div>
+                        <div class="card--rating">
+                            <div class="box">
+                                <p class="card--score">
+                                    <?php echo $recipe->getRating(); ?>
+                                    <i class="fa-solid fa-star"></i>
+                                </p>
+                                <p class="card--number">
+                                    <?php echo $recipe->getRatingUsersCount(); ?>
+                                    <i class="fa-solid fa-user"></i>
+                                </p>
+                            </div>
+                        </div>
+                        <form method="post" action="recipe_details.php">
+                            <input type="hidden" name="hidden_title" value="<?php echo $recipe->getTitle() ?>">
+                            <input type="hidden" name="hidden_user_id" value="<?php echo $recipe->getUserId() ?>">
+                            <button type="submit" name="recipe_details_submit" class="card--button">Details</button>
 
-                    </form>
-                    <form method="post" action="remove_favourite.php">
-                        <input type="hidden" name="secretid"
-                               value="<?php echo Recipe::getRecipeIdFromDataBase($recipe->getTitle())?>">
-                        <button type="submit" name="recipe_fav_remove" class="remove--button">Remove</button>
-                    </form>
-                </div>
-            </article>
-        </div>
+                        </form>
+                        <form method="post" action="../Includes/remove_favourite.php">
+                            <input type="hidden" name="secretid" value="<?php echo Recipe::getRecipeIdFromDataBase($recipe->getTitle()) ?>">
+                            <button type="submit" name="recipe_fav_remove" class="remove--button">Remove</button>
+                        </form>
+                    </div>
+                </article>
+            </div>
 
         <?php
         }
