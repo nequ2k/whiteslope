96df9e5c769b3a64e11583afc20e3bd37331dcde
Michał Cienkowski <120496051+MCienkowski@users.noreply.github.com>
2023-09-18 14:05:42 +0200

    Deleting unnecessary code

--- Views/users_search_results.php
+++ Views/users_search_results.php
@@ -13,7 +13,6 @@
 
     <section class="results">
 
-        <?php for($i = 0; $i < 8; $i++): ?>
 
         <?php
 
@@ -48,7 +47,6 @@
                 }
             }
             ?>
-        <?php endfor; ?>
 
     </section>
 </main>
